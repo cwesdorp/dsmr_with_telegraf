bdf080511ee791d5e6122d1ceab59b9aefe132a9
Chris Wesdorp <chriswesdorp@gmail.com>
2022-09-26 23:07:21 +0200

    Add delivered values to storage

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 image:
 	docker build --rm -t cwp/smt:1 .
 
+image-telegraf:
+	docker build --rm -t cwp/smt-telegraf:2 -f Dockerfile-telegraf .
+
 run:
-	docker run --rm -t --device=/dev/ttyUSB0 cwp/smt:1 
+	docker run --rm --device=/dev/ttyUSB0 -v "${PWD}/telegraf-influxdb_v2.conf:/etc/telegraf/telegraf.conf" --name=smart_meter -d cwp/smt-telegraf:2 
+
+run-test:
+	docker run --rm -t --device=/dev/ttyUSB0 -v "${PWD}/telegraf.conf:/etc/telegraf/telegraf.conf" --name=smart_meter -a STDOUT cwp/smt-telegraf:2 
--- dsmr.py
+++ dsmr.py
@@ -15,7 +15,7 @@ print("Starting DSMR Reader", file = sys.stderr)
 def printTelegram(telegram):
     t = str(telegram.P1_MESSAGE_TIMESTAMP)[:25]
     timestamp = datetime.strptime(t, "%Y-%m-%dT%H:%M:%S%z")
-    ts = int(timestamp.timestamp() * 1000 * 1000)
+    ts = int(timestamp.timestamp() * 1000 * 1000 * 1000)
 
     active_tarrif = int(telegram.ELECTRICITY_ACTIVE_TARIFF.value)
     print(F"electricity_active_tarrif value={active_tarrif} {ts}")
@@ -24,6 +24,14 @@ def printTelegram(telegram):
     print(F"electricity_current_use value={telegram.CURRENT_ELECTRICITY_USAGE.value} {ts}")
     print(F"electricity_voltage value={telegram.INSTANTANEOUS_VOLTAGE_L1.value} {ts}")
     print(F"electricity_current value={telegram.INSTANTANEOUS_CURRENT_L1.value} {ts}")
+    print(F"electricity_total_delivered,tarrif=1 value={telegram.ELECTRICITY_DELIVERED_TARIFF_1.value} {ts}")
+    print(F"electricity_total_delivered,tarrif=2 value={telegram.ELECTRICITY_DELIVERED_TARIFF_2.value} {ts}")
+    print(F"electricity_current_delivery value={telegram.CURRENT_ELECTRICITY_DELIVERY.value} {ts}")
+
+    t_gas = str(telegram.HOURLY_GAS_METER_READING.datetime)
+    timestamp_gas = datetime.strptime(t_gas, "%Y-%m-%d %H:%M:%S%z")
+    ts_gas = int(timestamp_gas.timestamp() * 1000 * 1000 * 1000)
+    print(F"gas_total_consumed value={telegram.HOURLY_GAS_METER_READING.value} {ts_gas}")
 
 if __name__ == '__main__':
     try:
